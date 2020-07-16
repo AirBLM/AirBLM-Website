e41c0a9ffa90e6fab4b5a84193e49d3ad5edf039
Emily <emilyk@nutranext.net>
2020-07-16 10:50:59 -0400

    removed unused code

--- airblm/src/Components/Button.js
+++ airblm/src/Components/Button.js
@@ -1,32 +1,14 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-
-
-const useStyles = makeStyles(theme => ({
-    modal: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    paper: {
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-    },
-}));
+import React from "react";
+import Button from "@material-ui/core/Button";
 
 const _Button = (props) => {
-    const classes = useStyles();
-
-    return (
-        <div>
-            <Button variant="contained" color="secondary">
-                {props.text}
-            </Button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Button variant="contained" color="secondary">
+        {props.text}
+      </Button>
+    </div>
+  );
+};
 
 export default _Button;
