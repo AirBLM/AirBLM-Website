04000c3dd6f411f7a0d6790656ed6fa2621f4791
yingsongsana <yingsongsana@gmail.com>
2020-07-16 20:27:11 -0400

    Display petitions in cards

--- airblm/src/Components/Petitions/Petitions.js
+++ airblm/src/Components/Petitions/Petitions.js
@@ -1,5 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { apiUrl, paths } from "../../apiConfig";
+import React, { useEffect, useState } from 'react';
+import { apiUrl, paths } from '../../apiConfig';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import './Petitions.css';
 
 const Petitions = (props) => {
   const [petitions, setPetitions] = useState([]);
@@ -14,11 +20,33 @@ const Petitions = (props) => {
       .catch(console.error);
   }, []);
 
-  return (
-    <div>
-      <h1>Petitions</h1>
-    </div>
-  );
-};
+    // console.log(petitions);
+
+    const petitionsJsx = petitions.map(petition => (
+        <Card variant="outlined" className="petition-card">        
+        <CardContent>
+            <Typography className="petition-name" color="textSecondary" gutterBottom href={petition.fields.Link}>
+                {petition.fields.Name}
+            </Typography>
+            <Typography className="petition-purpose" style={{ 'font-size': '10px' }}>
+                {petition.fields.Purpose}
+            </Typography>
+        </CardContent>
+        <CardActions>
+            <Button size="small" href={petition.fields.Link} target="_blank" style={{ 'margin-left': '190px', 'margin-top': '-10px'}}>
+                Take Action
+            </Button>
+        </CardActions>
+        </Card>
+    ))
+    
+    return(
+        <div className="petition-container">
+            {petitionsJsx}
+        </div>
+        )
+
+}
+>>>>>>> Display petitions in cards
 
 export default Petitions;
