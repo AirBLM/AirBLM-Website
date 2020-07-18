7d9feb90706aaa0863cf7b2bda8024ea870c1880
scottblender <57569396+scottblender@users.noreply.github.com>
2020-07-18 00:25:58 -0400

    Hosting

--- airblm/src/App.js
+++ airblm/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Routes from "./routes";
-
+import useLockBodyScroll from './useLockBodyScroll'
 function App() {
+  useLockBodyScroll();
   return (
     <Router>
       <div className="App">
